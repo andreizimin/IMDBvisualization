2ae2b73b1c6649300aace620b3bc6f83f2324a9d
andreizimin <andrei.zimin@gmail.com>
2023-10-09 00:38:10 -0700

    third commit

--- main.py
+++ main.py
@@ -1,5 +1,78 @@
-from config import OMDB_API_KEY
-print(OMDB_API_KEY)
+import pandas as pd
 
+df = pd.read_csv('/Users/zimina/PycharmProjects/IMDBvisualization/movies.csv')
 
+import networkx as nx
+import plotly.graph_objects as go
 
+# Graph
+G = nx.from_pandas_edgelist(df, 'source', 'target')
+
+# Get node positions
+pos = nx.get_node_attributes(G, 'pos')
+
+# Extract positions
+d = []
+for node in G.nodes():
+    x, y = pos[node]
+    d.append((x, y))
+
+# Create edges
+edge_x = []
+edge_y = []
+for edge in G.edges():
+    x0, y0 = pos[edge[0]]
+    x1, y1 = pos[edge[1]]
+    edge_x.extend([x0, x1, None])
+    edge_y.extend([y0, y1, None])
+
+# Create edge trace
+edge_trace = go.Scatter(
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines'
+)
+
+# Create node trace
+node_trace = go.Scatter(
+    x=[pos[i][0] for i in G.nodes()],
+    y=[pos[i][1] for i in G.nodes()],
+    mode='markers+text',
+    hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        colorscale='YlGnBu',
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'
+        ),
+        line_width=2
+    )
+)
+
+# Update node hover information
+node_adjacencies = []
+node_text = []
+for node, adjacencies in enumerate(G.adjacency()):
+    node_adjacencies.append(len(adjacencies[1]))
+    node_text.append('# of connections: ' + str(len(adjacencies[1])))
+
+node_trace.marker.color = node_adjacencies
+node_trace.text = node_text
+
+# Create the figure
+fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='Network Graph',
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=0, l=0, r=0, t=40),
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+
+fig.show()
